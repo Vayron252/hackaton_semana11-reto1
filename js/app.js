34fe9563121349971f31ffcc5514a4f74a2b59c2
Marco Alarcon <markinsh0@gmail.com>
2023-09-30 12:34:26 -0500

    Commit Calculadora

---
+++ js/app.js
@@ -0,0 +1,53 @@
+let operacionEscrita = '', operadorPresionado = '', escribir = true;
+const txtResultado = document.getElementById('txtResultado');
+const botonesNumero = document.querySelectorAll('.numero'), botonesAccion = document.querySelectorAll('.boton--accion');
+
+// const botonera = document.querySelector('.botonera');
+// console.log(botonera);
+// botonera.addEventListener('click', (e) => {
+//     console.log(e.target.textContent);
+// });
+
+const escribirOperacion = (valor) => {
+    operacionEscrita += valor;
+    txtResultado.value = operacionEscrita;
+}
+
+const limpiarOperacion = () => {
+    escribir = true;
+    operacionEscrita = '';
+    operadorPresionado = '';
+    txtResultado.value = '';
+}
+
+const mostrarResultado = () => {
+    escribir = false;
+    let arregloEscrito = operacionEscrita.split(operadorPresionado);
+    let numero1 = parseFloat(arregloEscrito[0]), numero2 = parseFloat(arregloEscrito[1]);
+    if (operadorPresionado == '+') txtResultado.value = numero1 + numero2;
+    if (operadorPresionado == '-') txtResultado.value = numero1 - numero2;
+    if (operadorPresionado == '*') txtResultado.value = numero1 * numero2;
+    if (operadorPresionado == '/') txtResultado.value = numero1 / numero2;
+}
+
+botonesNumero.forEach(boton => {
+    boton.addEventListener('click', () => {
+        if (escribir) escribirOperacion(boton.textContent);
+    });
+});
+
+botonesAccion.forEach(boton => {
+    boton.addEventListener('click', () => {
+        let contenidoBoton = boton.textContent;
+        if (contenidoBoton == 'AC') {
+            limpiarOperacion();
+            return;
+        } else if (contenidoBoton == '=') {
+            mostrarResultado();
+            return;
+        }else if (escribir) {
+            operadorPresionado = contenidoBoton;
+            escribirOperacion(boton.textContent);
+        }
+    });
+});
